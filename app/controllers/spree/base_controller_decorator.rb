a0b998d9e62e423685e9530c431df610776311fc
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2016-02-29 01:52:20 +0000

    improved

--- app/controllers/spree/base_controller_decorator.rb
+++ app/controllers/spree/base_controller_decorator.rb
@@ -1,26 +1,13 @@
 Spree::BaseController.class_eval do
 
-  # prepend_before_filter :redirect_supplier
+  prepend_before_filter :redirect_supplier
 
-  before_action :authorize_supplier
+  private
 
-  # private
-
-  # def redirect_supplier
-  #   if ['/admin', '/admin/authorization_failure'].include?(request.path) && try_spree_current_user.try(:supplier)
-  #     redirect_to '/admin/shipments' and return false
-  #   end
-  # end
-  protected
-
-  def authorize_supplier
-    if respond_to?(:model_class, true) && model_class
-      record = model_class
-    else
-      record = controller_name.to_sym
+  def redirect_supplier
+    if ['/admin', '/admin/authorization_failure'].include?(request.path) && try_spree_current_user.try(:supplier)
+      redirect_to '/admin/shipments' and return false
     end
-    authorize! :supplier, record
-    authorize! action, record
   end
 
 end
