1ce47a5639a4eec4e6dcfbd6ec3d59a465bd8bf7
Alex Mirkhaydarov <alex.mirkhaydarov@gmail.com>
2016-03-04 01:40:56 +0000

    added ability for supplier to edit and update created products

--- app/models/spree/supplier_ability.rb
+++ app/models/spree/supplier_ability.rb
@@ -12,14 +12,14 @@ module Spree
         can [:admin, :read, :stock], Spree::Product do |product|
           product.supplier_ids.include?(user.supplier_id)
         end
-        can [:admin, :index, :manage, :create], Spree::Product
+        can [:admin, :index, :manage, :create, :update, :edit], Spree::Product
         can [:admin, :manage], Spree::Image do |image|
           image.viewable.product.supplier_ids.include?(user.supplier_id)
         end
         can :create, Spree::Image
         can [:admin, :manage, :read, :ready, :ship], Spree::Shipment, order: { state: 'complete' }, stock_location: { supplier_id: user.supplier_id }
         can [:admin, :create, :update], :stock_items
-        can [:admin, :manage], Spree::StockItem, stock_location_id: user.supplier.stock_locations.pluck(:id)
+        can [:admin, :manage, :create, :update], Spree::StockItem, stock_location_id: user.supplier.stock_locations.pluck(:id)
         can [:admin, :manage], Spree::StockLocation, supplier_id: user.supplier_id
         can :create, Spree::StockLocation
         can [:admin, :manage], Spree::StockMovement, stock_item: { stock_location_id: user.supplier.stock_locations.pluck(:id) }
