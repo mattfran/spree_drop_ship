5a04e42c00abc402fe54bd67e133f22a96bb2085
Matt Francois <info@journeytech.ca>
2016-04-17 20:35:22 +0200

    removed frontend js and css

--- lib/generators/spree_drop_ship/install/install_generator.rb
+++ lib/generators/spree_drop_ship/install/install_generator.rb
@@ -4,12 +4,12 @@ module SpreeDropShip
       class_option :auto_run_migrations, :type => :boolean, :default => false
 
       def add_javascripts
-        append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_drop_ship\n"
+        # append_file 'vendor/assets/javascripts/spree/frontend/all.js', "//= require spree/frontend/spree_drop_ship\n"
         append_file 'vendor/assets/javascripts/spree/backend/all.js', "//= require spree/backend/spree_drop_ship\n"
       end
 
       def add_stylesheets
-        inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_drop_ship\n", :before => /\*\//, :verbose => true
+        # inject_into_file 'vendor/assets/stylesheets/spree/frontend/all.css', " *= require spree/frontend/spree_drop_ship\n", :before => /\*\//, :verbose => true
         inject_into_file 'vendor/assets/stylesheets/spree/backend/all.css', " *= require spree/backend/spree_drop_ship\n", :before => /\*\//, :verbose => true
       end
 
